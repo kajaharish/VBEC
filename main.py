d7524e09baa927379bf1296274b17025a22b877b
abhisheksinghchaudhary77@gmail.com <abhisheksinghchaudhary77@gmail.com>
2018-07-30 20:36:37 +0530

    main ready

--- main.py
+++ main.py
@@ -3,30 +3,25 @@ from gtts import gTTS
 import time
 import os
 
-
-def speak(audioString):
-    print(audioString)
-    tts = gTTS(text = audioString, lang = 'en')
-    filename = "audio.mp3"
-    tts.save(filename)
-    os.system("mpg321 audio.mp3")
-    os.remove(filename)
-
-def mainMenu():
-    mainMenu = ["1. Login", "2. Sign Up"]
-    speak("What do you want to do?")
-    speak("Speak the desired number for choice.")
-    for line in mainMenu:
-        speak(line)
+class main:
 
 
-def subMenu():
-    subMenu = ["1. Compose Mail", "2. Inbox", "3. Receive New Mails", "4. Goto Sent Mails"]
-    speak("Speak the desired number for choice.")
-    for line in subMenu():
-        speak(line)
+    def speak(self, audioString):
+        print(audioString)
+        self.tts = gTTS(text = audioString, lang = 'en')
+        self.filename = "audio.mp3"
+        self.tts.save(self.filename)
+        os.system("mpg321 audio.mp3")
+        #os.remove(self.filename)
+
+    def menu(self):
+        self.options = ["1. Compose Mail", "2. Inbox", "3. Receive New Mails", "4. Goto Sent Mails"]
+        self.speak("Speak the desired number for what you want to do.")
+        for choice in self.options:
+            self.speak(choice)
 
 #initialization
 #time.sleep(2)
-speak("Hello, Welcome to Voice Based Mail Client")
-mainMenu()
+spk = main()
+spk.speak("Hello, Welcome to TalkMail.")
+spk.menu()
